ade3677042e2bb9a8e15b0dc0ced5e401db7305a
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-01-04 15:44:36 +0900

    #3 - docs: 초기 directory 설정

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    html,body{
+        padding: 0;
+        margin: 0;
+      };
+
+      * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      }
+
+    li{
+      list-style:none;
+    }
+
+    img{
+      border: none;
+      vertical-align: top;
+    }
+
+  a {
+    color: initial;
+    text-decoration: none;
+  }
+
+  input,
+  textarea {
+    padding: 0;
+    outline: none;
+    border: 1px solid #000000;
+    border-radius: 0;
+    box-shadow: none;
+  }
+
+  button {
+    padding: 0;
+    border: none;
+    border-radius: 0;
+    box-shadow: none;
+    background-color: inherit;
+    cursor: pointer;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>asdf</div>
+    </>
+  );
+}
+
+export default App;
