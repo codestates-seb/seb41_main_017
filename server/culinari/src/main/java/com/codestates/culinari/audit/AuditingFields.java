70d653ee7f70a7c4acfcd56cc015eac046fe103f
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2023-01-06 11:26:09 +0900

    #4 - style: `AudtiingFields` 수정

--- server/culinari/src/main/java/com/codestates/culinari/audit/AuditingFields.java
+++ server/culinari/src/main/java/com/codestates/culinari/audit/AuditingFields.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
