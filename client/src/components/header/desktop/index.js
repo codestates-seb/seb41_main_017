87afab11fd315ab8c56338fd6004d2c217ec5926
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-01-07 17:05:32 +0900

    #5 - feat : 마이페이지 구조 변경

---
+++ client/src/components/header/desktop/index.js
@@ -0,0 +1,159 @@
+import styled from "styled-components";
+import { BsFillPersonFill, BsCart4, BsList, BsSearch } from "react-icons/bs";
+
+
+const Layout = styled.div`
+  
+  // 화면사이즈 수정 ---
+  width: 1050px;
+  margin: 0 auto;
+  // 화면사이즈 수정 ---
+
+  .flex {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .top {
+    padding: 7px 10px;
+    gap: 15px;
+    justify-content: flex-end;
+    background-color: #ffc3b1;
+  }
+
+  .mid {
+    margin-top: 20px;
+
+    .logo{
+        margin-right: 10px;
+        img {
+            width: 40px;
+          }
+    }
+   
+    .myIcons{
+        gap: 10px;
+    }
+
+    .icons{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+    
+        .icon{
+            font-size: 30px;
+            margin-bottom: 2px;     
+      }
+    }
+
+    .serach{
+        display: flex;
+        border: 1px solid #ffc3b1;
+        width: 400px;
+        height: 48px;
+        border-radius: 10px;
+        justify-content: space-around;
+        align-items: center;
+        margin: 0 10px;
+
+        
+        
+        input{
+            flex:5;
+            border: none;
+            margin-left: 10px;
+        }
+
+        button{
+            flex: 1;
+            font-size: 20px;
+            color: #ffc3b1;
+            
+        }
+    }
+  }
+
+.bottom{
+    border-bottom: 1px solid #A9A9A9;
+    margin-top: 20px;
+      .GNB{
+        flex:1;
+    
+        ul{
+            display:flex;
+            justify-content: space-around;
+            align-items: center;
+
+           li{
+            flex:1;
+            text-align: center;
+            padding: 5px 5px 5px 0;
+            
+            
+           }
+           .category{
+            display:flex;
+            align-items: center;
+            // text-align: left;
+            
+           }
+        }
+      }
+}
+`;
+
+function Header() {
+    
+
+  return (
+    <Layout>
+        {/* 상단라인 */}
+      <div className="top flex">
+        <div>로그인</div>
+        <div>회원가입</div>
+        <div>고객센터</div>
+      </div>
+
+      {/* 중간라인 */}
+      <div className="mid flex">
+        <div className="logo">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/DaangnMarket_logo.png/800px-DaangnMarket_logo.png" alt="logo"></img>
+        </div>
+        {/* 검색창 */}
+        <div className="serach">
+          <input placeholder="검색어를 입력해주세요"></input>
+          <button><BsSearch/></button>
+        </div>
+        <div className="myIcons flex">
+            {/* 아이콘 */}
+          <div className="icons">
+            <BsFillPersonFill className="icon"/>
+            {/* <span>마이페이지</span> */}
+          </div>
+          <div className="icons">
+            <BsCart4 className="icon"/>
+            {/* <span>장바구니</span> */}
+          </div>
+        </div>
+      </div>
+      {/* 메뉴 탭 */}
+      <div className="bottom flex">
+        <div className="GNB">
+          <ul>
+            <li className="category">
+                <span><BsList size="25"/></span>
+                <span>카테고리</span>
+            </li>
+            <li><span>홈</span></li>
+            <li>신상품</li>
+            <li>베스트</li>
+            <li>이벤트</li>
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Header;
--- client/src/pages/mypage/index.js
+++ client/src/pages/mypage/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { GoChevronRight } from "react-icons/go";
-import { Route, Routes, BrowserRouter, Link, NavLink} from "react-router-dom";
+import { Route, Routes, BrowserRouter, NavLink } from "react-router-dom";
 import Itemreview from "./itemreview";
 import Orderitem from "./orderitem";
 import ProductInquire from "./productInquire";
@@ -8,85 +8,230 @@ import Userinfo from "./userinfo";
 import UserinfoEdit from "./userinfoEdit";
 import Wish from "./wish";
 
-const Layout = styled.div`
-  margin: 0 auto;
-  // 수정해야할부분 부모크기에 맞게
+const Mycard = styled.div`
+  // background-color: #ffc3b1;
+  background-color: rgb(255,224,214);
+  // 부모와 맞게 수정
   width: 1050px;
+  margin: 0 auto;
   display: flex;
-  gap: 20px;
+  text-align: center;
+  margin-top: 15px;
 
-  .nav {
-    flex: 1;
-    h3 {
-      padding: 20px;
-    }
-    ul {
-      border: 1px solid rgb(242, 242, 242);
-    }
-    a {
+  .accountBox {
+    padding: 10px 0 0 30px;
+    flex-basis: 300px;
+    height: 212px;
+    display: flex;
+    flex-direction: column;
+
+    .acName {
       display: flex;
-      justify-content: space-between;
-      font-weight: bold;
-      align-items: center;
+      gap: 10px;
+      margin: 15px 0 35px 0;
+      span {
+        font-size: 24px;
+      }
+      // button{
+      //   width: 77px;
+      // }
+    }
+
+    .acEvent {
+      justify-content: left;
+      background-color: #fff7f5;
+      width: 195px;
       padding: 10px;
-      border-bottom: 1px solid rgb(242, 242, 242);
-      margin-bottom: -1px;
-      cursor: pointer;
-      color: rgb(102, 102, 90);
-      
-    }
-    .clickCo{
-      background-color: rgb(250, 250, 250);
-      color:rgb(95, 0, 128);
+      border-radius: 10px;
+
+      .event {
+        margin: 10px;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        & span:last-child {
+          color: red;
+        }
+      }
     }
   }
 
-  a:hover,
-  .light:hover{  
-    color:rgb(95, 0, 128);
-    background-color: rgb(250, 250, 250);
-  }
-  
-  .content {
-    flex: 4;
-    padding-top:20px;
-  }
+  .product {
+    display: flex;
+    flex: 1;
+    align-items: center;
 
+    .count{
+      color: white;
+      font-size: 50px;
+      margin: 20px 0;
+    }
+
+    & > div {
+      flex: 1;
+    }
+  }
 `;
 
+const Tap = styled.div`
+// 부모와 맞게 수정
+  margin: 0 auto;
+  width: 1050px;
+  text-align: left;
+  margin-top: 20px;
 
+  .nav {
+    
+
+    h3{
+      font-size: 20px;
+    }
+    
+    ul{
+      display:flex;
+      margin-top: 10px;
+
+      li{
+        flex:1;
+        display:flex;
+
+        button{
+          flex:1;
+          margin: 0 2px;
+        }
+
+      }
+      
+    }
+    
+  }
+
+  .content {
+    border-top: 1px solid #D7D7D7;
+    margin-top: 10px;
+    padding: 10px;
+  }
+`;
+
+const Colbtn = styled.button`
+  background-color: #ffffff;
+  border: 1px solid #ff6767;
+  border-radius: 10px;
+  padding: 4px 8px;
+  // width: 100%;
+  color: #ff6767;
+  cursor: pointer;
+  font-size: 12px;
+  flex-shrink: 0;
+
+  a{
+    color: #ff6767;
+  }
+`;
 
 function Mypage() {
   const tap = [
-    {"주문 내역":"/orderitem"},
-    {"찜한 상품":"/itemreview"},
-    {"상품 후기":"/productInquire"},
-    {"상품 문의":"/userinfo"},
-    {"유저 정보":"/userinfoEdit"},
-    {"개인 정보 수정":"/wish"}
-  ]
+    { "내 정보": "/userinfoEdit" },
+    { "배송지 설정": "#" },
+    { "배송 조회": "#" },
+    { "주문 목록 조회": "/orderitem" },
+    { "내 쿠폰": "#" },
+    { "내 포인트": "#" },
+    { "자주 산 상품": "#" },
+    { "최근 본 상품": "#" },
+    { "작성한 후기": "/productInquire" },
+    { "내 문의": "/userinfo" },
+    
+  
+    // 회원카드 옆의 개인정보 수정은 내정보와 같은 페이지인가 ?
+    //  -> { "개인 정보 수정": "/wish" }
+    // 찜한상품 보기 카테고리가 없음
+    // -> { "찜한 상품": "/itemreview" }
+    // 카테고리에 맞는 파일들을 만들어야함 ~~~~
+  ];
 
   return (
     <BrowserRouter>
-      <Layout>
+      <Mycard>
+        <div className="accountBox">
+          <div className="acName">
+            <span>ID님의 회원카드</span>
+            <Colbtn>내 정보 수정</Colbtn>
+          </div>
+          <div className="acEvent">
+            <div className="event">
+              <span>내 포인트</span>
+              <span>
+                0P
+                <GoChevronRight />
+              </span>
+            </div>
+            <div className="event">
+              <span>내 쿠폰</span>
+              <span>
+                0장
+                <GoChevronRight />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="product">
+          <div>
+            <h2>배송중</h2>
+            <div className="count">
+              <span>0</span>
+            </div>
+            <Colbtn><a>배송조회</a></Colbtn>
+          </div>
+          <div>
+            <h2>주문 목록</h2>
+            <div className="count">
+              <span>0</span>
+            </div>
+            <Colbtn><a>주문 목록 조회</a></Colbtn>
+          </div>
+          <div>
+            <h2>최근 본 상품</h2>
+            <div className="count">
+              <span>0</span>
+            </div>
+            <Colbtn><a>조회</a></Colbtn>
+          </div>
+          <div>
+            <h2>자주 산 상품</h2>
+            <div className="count">
+              <span>0</span>
+            </div>
+            <Colbtn><a>조회</a></Colbtn>
+          </div>
+        </div>
+      </Mycard>
+      <Tap>
         <div className="nav">
-          <h3>마이컬리</h3>
+          <h3>마이페이지</h3>
           <ul>
-            {tap.map((el, idx)=>{
-              return(
-                <li key={idx}>
-                  <NavLink 
-                  to={Object.values(el)[0]}
-                  className={({isActive}) => (isActive ? "clickCo" : null)}
-                  >{Object.keys(el)}<GoChevronRight className="light" /></NavLink>
-                </li>
+            {tap.map((el, idx) => {
+              return (
+               <li>
+                 <Colbtn key={idx}>
+                  <NavLink
+                    to={Object.values(el)[0]}
+                    className={({ isActive }) => (isActive ? "clickCo" : null)}
+                  >
+                    {Object.keys(el)}
+                  </NavLink>
+                </Colbtn>
+               </li>
               );
             })}
           </ul>
         </div>
         <div className="content">
           <Routes>
-            {/* <Route path={tap[0]["주문내역"]} element={<Orderitem />} /> */}
+            {/*작업용 추후 삭제  */}
+            <Route path="/" element={<Orderitem />} />
             <Route path="/orderitem" element={<Orderitem />} />
             <Route path="/itemreview" element={<Itemreview />} />
             <Route path="/productInquire" element={<ProductInquire />} />
@@ -95,7 +240,7 @@ function Mypage() {
             <Route path="/wish" element={<Wish />} />
           </Routes>
         </div>
-      </Layout>
+      </Tap>
     </BrowserRouter>
   );
 }
