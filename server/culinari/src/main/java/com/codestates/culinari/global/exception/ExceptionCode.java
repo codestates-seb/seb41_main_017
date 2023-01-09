7ed59de05ed39fe88e79108d8cc1ed1c41c7e052
kurt <qodydgus97@kakao.com>
2023-01-09 14:53:39 +0900

    #9 - feat : User도메인 에러코드 추가

--- server/culinari/src/main/java/com/codestates/culinari/global/exception/ExceptionCode.java
+++ server/culinari/src/main/java/com/codestates/culinari/global/exception/ExceptionCode.java
@@ -4,7 +4,12 @@ import lombok.Getter;
 
 public enum ExceptionCode {
 
-    PRODUCT_NOT_FOUND(404,"Product not found");
+    PRODUCT_NOT_FOUND(404,"Product not found"),
+    USER_NOT_FOUND(404,"User not found"),
+    PROFILE_NOT_FOUND(404,"Profile not found"),
+    USERNAME_EXISTS(405, "Username exist"),
+    EMAIL_EXISTS(405, "Username exist");
+
 
     @Getter
     private int status;
