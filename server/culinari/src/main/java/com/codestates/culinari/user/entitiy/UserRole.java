5df326e1d06d9538ed73b6dac301aef9daedbc9a
kurt <qodydgus97@kakao.com>
2023-01-09 14:56:19 +0900

    #9 - refactor : 코드 리팩토링
    사용하지 않는 양방향 매핑, 변경된 ERD와 이름, 크기등을 일치하게 수정, 사용하지 않는 import 제거.

--- server/culinari/src/main/java/com/codestates/culinari/user/entitiy/UserRole.java
+++ server/culinari/src/main/java/com/codestates/culinari/user/entitiy/UserRole.java
@@ -1,15 +1,18 @@
 package com.codestates.culinari.user.entitiy;
 
-import com.codestates.culinari.audit.AuditingFields;
+import com.codestates.culinari.user.constant.RoleType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class UserRole extends AuditingFields {
+public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,7 +21,11 @@ public class UserRole extends AuditingFields {
     @Column(nullable = false)
     private RoleType role;
 
-    // 논의
+    @JsonBackReference
     @ManyToOne
     private Users user;
+
+    public static UserRole of(RoleType roleType, Users user) {
+        return new UserRole(null, roleType, user);
+    }
 }
--- server/culinari/src/main/java/com/codestates/culinari/user/entitiy/Users.java
+++ server/culinari/src/main/java/com/codestates/culinari/user/entitiy/Users.java
@@ -1,35 +1,39 @@
 package com.codestates.culinari.user.entitiy;
 
-import com.codestates.culinari.audit.AuditingFields;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Users extends AuditingFields {
+public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String userId;
+    @Column(nullable = false, length = 30)
+    private String username;
 
     @Column(nullable = false, length = 255)
     private String password;
 
-    // 논의 @ElementCollection
-    @OneToMany(mappedBy = "user")
-    private List<UserRole> userRoles = new ArrayList<>();//
+    @JsonManagedReference
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
+    private List<UserRole> userRoles = new ArrayList<>();
 
     @ManyToOne
     private Profile profile;
 
+    public static Users of(Long id, String username, String password, List<UserRole> userRoles, Profile profile) {
+        return new Users(id, username, password, userRoles, profile);
+    }
+
+    public void addUserRole(UserRole userRole){
+        this.userRoles.add(userRole);
+    }
 }
-//
-// @ElementCollection
