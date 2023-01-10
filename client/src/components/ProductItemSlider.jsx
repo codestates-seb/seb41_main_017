970603717705e095b270741a8333cd15e2024efa
Seonghun <albert5428@gmail.com>
2023-01-10 23:34:53 +0900

    [FE] 메인페이지 구성 컴포넌트 구현, 메인페이지 구현 (#29)
    * Update issue templates
    
    * Create pull_request_template.md
    
    * #10 - chore: react-slick dependencies 추가
    
    * #10 - feat: MainBanner 컴포넌트 구현
    
    * #10 - chore: 불필요한 태그 삭제
    
    * #10 - feat: ProductItem 컴포넌트 구현
    
    * #10 - feat: ProductItemListSlider 컴포넌트 구현
    
    * #10 - feat: 메인 페이지 구현
    
    * #10 - design: 버튼 호버 시 색 변경 효과 구현
    
    * #10 - design: 메인 배너 버튼 변경
    
    * #10 - design: ProductItemListSlider 버튼 css 변경
    
    1. 더 이상 왼쪽에 상품이 없을 때 왼쪽버튼을 숨김.
    2. 더 이상 오른쪽에 상품이 없을 때 오른쪽버튼을 숨김.
    
    * #10 - rename: ProductItemListSlider -> ProductItemSlider 수정
    
    * #10 - remove: 불필요한 파일 삭제
    
    * #10 - design: MainBanner css 변경
    
    1. 헤더의 width 맞춤
    2. 이전, 다음 버튼 위치 수정
    3. 배너 현재 페이지 박스 위치 수정
    
    * #10 - chore: CurrentPage -> PageBox 컴포넌트명 수정
    
    * #10 - chore: 메인 페이지 줄바꿈 수정
    
    Co-authored-by: Heo SeokJin <pooh6195@naver.com>

---
+++ client/src/components/ProductItemSlider.jsx
@@ -0,0 +1,165 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { ReactComponent as WhiteArrow } from "../assets/white_arrow.svg";
+import ProductItem from "./ProductItem";
+
+const PrevButtonContainer = styled.div`
+  position: absolute;
+  top: 97px;
+  left: -25px;
+  z-index: 3;
+  display: ${({ isFirstPage }) => (isFirstPage ? "none" : "block")};
+
+  &:hover {
+    path {
+      transition: 0.3s ease 0s;
+      fill: #fd6c40;
+    }
+  }
+
+  &:not(:hover) {
+    path {
+      transition: 0.3s ease 0s;
+      fill: black;
+    }
+  }
+`;
+
+function PrevButton({ isFirstPage, onClick }) {
+  return (
+    <PrevButtonContainer isFirstPage={isFirstPage} onClick={onClick}>
+      <WhiteArrow />
+    </PrevButtonContainer>
+  );
+}
+
+const NextButtonContainer = styled.div`
+  position: absolute;
+  top: 100px;
+  right: -25px;
+  z-index: 3;
+  transform: rotate(180deg);
+  display: ${({ isLastPage }) => (isLastPage ? "none" : "block")};
+
+  &:hover {
+    path {
+      transition: 0.3s ease 0s;
+      fill: #fd6c40;
+    }
+  }
+
+  &:not(:hover) {
+    path {
+      transition: 0.3s ease 0s;
+      fill: black;
+    }
+  }
+`;
+
+function NextButton({ isLastPage, onClick }) {
+  return (
+    <NextButtonContainer isLastPage={isLastPage} onClick={onClick}>
+      <WhiteArrow />
+    </NextButtonContainer>
+  );
+}
+
+function ProductItemSlider() {
+  const slidesToShow = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const productArr = [
+    {
+      id: 0,
+      name: "[비비드키친] 저칼로리 숯불매콤치킨 소스",
+      price: 3980,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1657781547680l0.jpg",
+    },
+    {
+      id: 1,
+      name: "[파제르 핀란드 시그니처 코촐릿 4종]",
+      price: 12000,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1607930413465l0.jpg",
+    },
+    {
+      id: 2,
+      name: "[카스0.0] 논알콜 음료 8캔 & 헌터즈트러플 감자칩 2개",
+      price: 9990,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1658317867100l0.jpg",
+    },
+    {
+      id: 3,
+      name: "한뿌리 수삼 45g",
+      price: 3490,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1576555719180l0.jpg",
+    },
+    {
+      id: 4,
+      name: "[순백수] 하노키 우디 디퓨저 세트 (본품 150ml)",
+      price: 26900,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1655455835441l0.jpeg",
+    },
+    {
+      id: 5,
+      name: "[상하목장] 얼려먹는 아이스크림 초코",
+      price: 5200,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/16499051856l0.jpg",
+    },
+    {
+      id: 6,
+      name: "[창화당] 육즙 팡팡 소룡포",
+      price: 8830,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1548292714543l0.jpg",
+    },
+    {
+      id: 7,
+      name: "[만토바] 비보 오가닉 엑스트버진 스프레이 화이트트러플향 200ml",
+      price: 3980,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1641272547982l0.jpg",
+    },
+    {
+      id: 8,
+      name: "[설치배송][LG전자] 울트라 HDTV (스탠드형)",
+      price: 633800,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1634802297108l0.jpg",
+    },
+    {
+      id: 9,
+      name: "[하선정] 김밥 단무지",
+      price: 2380,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/1657529753879l0.jpg",
+    },
+    {
+      id: 10,
+      name: "[제니튼] 닥터제니 저불소 어린이 치약 60g",
+      price: 6300,
+      image: "https://img-cf.kurly.com/cdn-cgi/image/quality=85,width=400/shop/data/goods/16505299970l0.jpg",
+    },
+  ];
+
+  const isFirstPage = () => currentPage === 1;
+  const isLastPage = () => currentPage === productArr.length - slidesToShow + 1;
+
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: slidesToShow,
+    prevArrow: <PrevButton isFirstPage={isFirstPage()} />,
+    nextArrow: <NextButton isLastPage={isLastPage()} />,
+    beforeChange: (_, index) => setCurrentPage(index + 1),
+  };
+
+  return (
+    <Slider {...settings}>
+      {productArr.map((element) => {
+        return <ProductItem element={element} key={element.id}></ProductItem>;
+      })}
+    </Slider>
+  );
+}
+
+export default ProductItemSlider;
