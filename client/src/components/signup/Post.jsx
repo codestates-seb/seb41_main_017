b2c51c84f21f7ae3037f54c77f8d621369ecc826
junpotatoes <mukdol1127@naver.com>
2023-01-10 22:19:30 +0900

    #6 - feat: Post 컴포넌트 구현

---
+++ client/src/components/signup/Post.jsx
@@ -0,0 +1,45 @@
+import DaumPostcode from "react-daum-postcode";
+import styled from "styled-components";
+// import "./post.css";
+
+const AddressModal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Post = ({ setAddress, setModalOpen }) => {
+  const complete = (data) => {
+    let fullAddress = setAddress(data.address);
+    let extraAddress = "";
+    setModalOpen(false);
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+    console.log(data);
+    // console.log(fullAddress);
+    // console.log(data.zonecode);
+
+    // setAddress.setcompany({
+    //   ...setAddress.company,
+    //   address: fullAddress,
+    // });
+  };
+
+  return (
+    <>
+      <AddressModal>
+        <DaumPostcode className="postmodal" autoClose onComplete={complete} />
+      </AddressModal>
+    </>
+  );
+};
+
+export default Post;
