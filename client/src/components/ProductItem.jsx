3ee505de82d36439d52231b55cef1d4083c3b040
jsh3418 <albert5428@gmail.com>
2023-01-11 21:27:25 +0900

    #42 - design: ProductItem css 수정
    1. 이미지 호버 시 이미지 확대
    2. 상품명 2줄 넘어갔을 때 ...으로 자름

--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -1,30 +1,36 @@
 import styled from "styled-components";
 
 const Item = styled.div`
-  width: 150px;
   margin: 0 auto;
   padding: 10px;
   font-size: 14px;
-  font-weight: 400;
+
+  .image_wrapper {
+    overflow: hidden;
+    margin-bottom: 6px;
+  }
 
   .name {
+    margin-bottom: 5px;
     display: -webkit-box;
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
-    height: 40px;
     line-height: 20px;
     overflow: hidden;
     text-overflow: ellipsis;
   }
-  .name,
+
   .price {
     margin-bottom: 5px;
   }
 
   img {
     width: 100%;
-    height: 150px;
-    margin-bottom: 6px;
+    transition: 1s;
+
+    &:hover {
+      transform: scale(1.05);
+    }
   }
 `;
 
@@ -32,9 +38,13 @@ function ProductItem({ element }) {
   return (
     <Item>
       <a>
-        <img src={element.image}></img>
+        <div className="image_wrapper">
+          <img src={element.image}></img>
+        </div>
+
         <div>
           <h3 className="name">{element.name}</h3>
+
           <div className="price">{element.price.toLocaleString()}원</div>
         </div>
       </a>
