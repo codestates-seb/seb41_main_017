f9c5bb2c9c6e560f316be961337406461b09d31e
junpotatoes <mukdol1127@naver.com>
2023-01-11 17:16:20 +0900

    #6 - misc: merge 병합

--- server/culinari/src/main/java/com/codestates/culinari/product/entitiy/ProductReviewLike.java
+++ server/culinari/src/main/java/com/codestates/culinari/product/entitiy/ProductReviewLike.java
@@ -6,6 +6,10 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -15,12 +19,27 @@ public class ProductReviewLike extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 논의
+    @Setter
+    private Long likeNum = 0L;
+
+    @Setter
     @OneToOne
+    @JoinColumn(name = "product_review_id")
     private ProductReview productReview;
 
-    //논의
-    @ManyToOne
-    private Profile profile;
+    @ElementCollection
+    private final List<Long> productReviewProfileIds = new ArrayList<>();
+
+    public ProductReviewLike(Long likeNum, ProductReview productReview){
+        this.likeNum = likeNum;
+        this.productReview = productReview;
+    }
+    public static ProductReviewLike of(Long likeNum, ProductReview productReview){
+        return new ProductReviewLike(likeNum, productReview);
+    }
+    public void setProductReviewProfileIds(Long profileId){
+        this.productReviewProfileIds.add(profileId);
+
+    }
 
 }
---
+++ server/culinari/src/main/java/com/codestates/culinari/product/repository/ProductReviewLikeRepository.java
@@ -0,0 +1,8 @@
+package com.codestates.culinari.product.repository;
+
+import com.codestates.culinari.product.entitiy.ProductReviewLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductReviewLikeRepository extends JpaRepository<ProductReviewLike, Long> {
+
+}
