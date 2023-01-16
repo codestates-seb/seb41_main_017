4ad60a789d23e0bde58d2c2a890cec4ddc4bb3b1
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-01-16 10:35:47 +0900

    #20 - feat : button component

--- client/src/components/BasicButton.jsx
+++ client/src/components/BasicButton.jsx
@@ -1,13 +1,12 @@
 import styled from "styled-components";
-// 해당컴포넌트는 div를 감싸서 써야 글씨가 정렬됨
+
+
 const BasicButton = ({href, children, font, radius, p_height, p_width}) =>{
     const styles = {font, radius, p_height, p_width};
     return(
-        <>
             <Colbtn {...styles} href={href}>
                 {children}
             </Colbtn>
-        </>
     )
 }
 
@@ -16,8 +15,7 @@ BasicButton.defaultProps = {
     font:"12",
     radius:"4",
     p_height:"4",
-    p_width:"7",
-    
+    p_width:"7", 
 }
 
 const Colbtn = styled.a`
