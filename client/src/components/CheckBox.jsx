f76a47fd36f1db459cdeb46f58cff9602e0b991e
jsh3418 <albert5428@gmail.com>
2023-01-18 23:20:16 +0900

    #80 - refactor: CheckBox 컴포넌트 불필요한 태그, 중괄호 삭제

--- client/src/components/CheckBox.jsx
+++ client/src/components/CheckBox.jsx
@@ -4,30 +4,26 @@ import { ReactComponent as CheckBoxIcon } from "../assets/check-box-icon.svg";
 const Label = styled.label`
   margin-right: 12px;
 
-   {
-    svg {
-      width: ${({ size }) => size};
-      height: ${({ size }) => size};
+  svg {
+    width: ${({ size }) => size};
+    height: ${({ size }) => size};
 
-      g {
-        fill: ${({ isChecked }) => (isChecked ? "#fd6c40" : "none")};
-        stroke: ${({ isChecked }) => (isChecked ? "#ffffff" : "#DDD")};
-      }
+    g {
+      fill: ${({ isChecked }) => (isChecked ? "#fd6c40" : "none")};
+      stroke: ${({ isChecked }) => (isChecked ? "#ffffff" : "#DDD")};
+    }
 
-      circle {
-        stroke: ${({ isChecked }) => (isChecked ? "#fd6c40" : "#DDD")};
-      }
+    circle {
+      stroke: ${({ isChecked }) => (isChecked ? "#fd6c40" : "#DDD")};
     }
   }
 `;
 
 function CheckBox({ isChecked, size = "35px" }) {
   return (
-    <>
-      <Label isChecked={isChecked} size={size}>
-        <CheckBoxIcon></CheckBoxIcon>
-      </Label>
-    </>
+    <Label isChecked={isChecked} size={size}>
+      <CheckBoxIcon></CheckBoxIcon>
+    </Label>
   );
 }
 
