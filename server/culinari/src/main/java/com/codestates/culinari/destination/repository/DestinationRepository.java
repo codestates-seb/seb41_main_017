fb7ce7f5b06984c98d13cfe4ba852e41e8469e51
kurt <qodydgus97@kakao.com>
2023-01-19 00:21:15 +0900

    #72 - feat : 배송지 Repository 추가
    처음엔 JPQL로 처리했지만 오류가 발생해 주석처리 해두었다.
    코드리뷰시간에 같이 오류를 확인하고 변경할 예정.

---
+++ server/culinari/src/main/java/com/codestates/culinari/destination/repository/DestinationRepository.java
@@ -0,0 +1,16 @@
+package com.codestates.culinari.destination.repository;
+
+import com.codestates.culinari.destination.entity.Destination;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface DestinationRepository extends JpaRepository<Destination, Long> {
+
+    List<Destination> findAllByProfile_Id(@Param("profileId") Long profileId);
+
+//    @Query("select d from Destination d where d.profile.id = :profileId")
+//    List<Destination> findAllByProfileId(@Param("profileId") Long profileId);
+}
