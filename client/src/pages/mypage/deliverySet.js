3ec67e25b9c87636314cb153a254139e725f2a57
장준익 <jangjun-ig@jangjun-ig-ui-MacBookPro.local>
2023-01-19 01:21:01 +0900

    #20 - design: deliverySet component 디자인변경

--- client/src/pages/mypage/deliverySet.js
+++ client/src/pages/mypage/deliverySet.js
@@ -1,126 +1,106 @@
 import styled from "styled-components";
 import Mypagebtn from "../../components/BasicButton";
-import CheckBox from "../../components/CheckBox"
+import CheckBox from "../../components/CheckBox";
+import Mypagehead from "../../components/MypageHead";
+import ListLayout from "../../components/ListLayout";
+import Modalbutton from "../../components/Modalbutton";
 
-const Layout = styled.div`
-  margin: 0 80px;
-
-  .header {
+const Classlist = styled.div`
+  .check_box {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 5px;
-
-    .cerate {
-      display: flex;
-      align-items: center;
-      gap: 3px;
-      font-size: 15px;
-    }
-  }
-`;
-
-const List = styled.div`
-  margin-bottom: 20px;
-  border: 1px solid #d7d7d7;
-  display: flex;
-  align-items: center;
-  padding: 10px 20px;
-
-  .save {
-    font-size: 30px;
-    padding-right: 20px;
-  }
-
-  .list_Detail {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    align-items:flex-start;
     justify-content: center;
-    padding: 8px 12px;
-
-    .title {
-      display: flex;
-      gap: 10px;
-  
-      span {
-        color: #ff6767;
-        vertical-align: text-bottom;
-      }
-  
+    align-items: center;
+    flex-basis: 60px;
+    margin-right:10px;
+    label {
+      margin: 0;
     }
+  }
 
-
-    .user_Info{
-      margin-top: 15px;
-      margin-bottom: 5px;
+  .list_content {
+    flex: 1;
+    .list_in_top{
+      padding-bottom: 15px;
+      font-size: 15px;
       span{
-        margin-right: 17px;
+        color:#FF6767;
+        padding-right: 5px;
       }
     }
-    .user_Address{
-      font-size: 20px;
-    }
-    .user_Message{
-      margin-top: 15px;
+
+    .list_in_mid{
+      padding-bottom: 15px;
       span{
-        margin-right: 17px;
+        padding-right: 15px;
+      }
+      p{
+        padding-top:5px;
+      }
+    }
+
+    .list_in_footer{
+
+      & span:first-child{
+        color:#676767;
+      }
+
+      span{
+        padding-right: 15px;
       }
     }
   }
-  .list_Btns {
-    display: flex;
-    gap: 10px;
+
+  .list_button{
+    flex-basis: 120px;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    gap: 5px;
   }
 `;
 
 function Addressset() {
   return (
-    <Layout>
-      <div className="header">
-        <div>
-          <span>배송지 설정</span>
-        </div>
-        <div className="cerate">
-          <Mypagebtn radius={50} p_height={3} p_width={5}>
-            +
-          </Mypagebtn>
-          <h5>배송지 추가</h5>
-        </div>
-      </div>
-      <List>
-        <div className="save">
-          <CheckBox isChecked={true}></CheckBox>
-        </div>
-        <div className="list_Detail">
-          <div className="title">
-            <div>
-              <span>배송지 이름입니다</span>
-            </div>
-            <div>
-              <Mypagebtn radius={5} p_width={10} p_height={1}>
-                기본
+    <Mypagehead title={"배송지 설정"} side_title={<Modalbutton text={"배송지 추가"}/>}>
+      <Classlist>
+        <ListLayout
+          radius={"5"}
+          display={"flex"}
+          justifyContent={"space-between"}
+          padding_width={"10px"}
+          padding_height={"20px"}
+        >
+          <div className="check_box">
+            <CheckBox isChecked={true}></CheckBox>
+          </div>
+          <div className="list_content">
+            <div className="list_in_top">
+              <span>{"배송지 이름입니다"}</span>
+              <Mypagebtn radius={"5"} p_width={"10"} p_height={"1"}>
+                {"기본"}
               </Mypagebtn>
             </div>
+            <div className="list_in_mid">
+              <span>{"최준호"}</span>
+              <span>{"010-0000-0000"}</span>
+              <p>{"경기도 수원시 권선구 세류1동 256 - 32 201호"}</p>
+            </div>
+            <div className="list_in_footer">
+              <span>{"배송메세지"}</span>
+              <span>{"문 앞에 놔주세요"}</span>
+            </div>
           </div>
-          <div className="user_Info"><span>최준호</span><span>010-000-000</span></div>
-          <div className="user_Address"><span>경기도 수원시 권선구 세류1동 256-32 201호</span></div>
-          <div className="user_Message">
-            <span>배송메세지</span>
-            <span>문 앞에 놔주세요</span>
+          <div className="list_button">
+            <Mypagebtn p_width={"15"} p_height={"5"}>
+              수정
+            </Mypagebtn>
+            <Mypagebtn p_width={"15"} p_height={"5"}>
+              삭제
+            </Mypagebtn>
           </div>
-        </div>
-        <div className="list_Btns">
-          <Mypagebtn p_width={10} p_height={4}>
-            수정
-          </Mypagebtn>
-          <Mypagebtn p_width={10} p_height={4}>
-            삭제
-          </Mypagebtn>
-        </div>
-      </List>
-    </Layout>
+        </ListLayout>
+      </Classlist>
+    </Mypagehead>
   );
 }
 
