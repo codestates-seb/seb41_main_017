972dca50b1e4d75728e683b80d477f9808c271ea
sGOM <pooh6195@naver.com>
2023-01-23 15:12:24 +0900

    #88 - 테스트를 위한 `Stub` 데이터 작성

--- server/culinari/src/main/java/com/codestates/culinari/payment/dto/response/PaymentCardResponse.java
+++ server/culinari/src/main/java/com/codestates/culinari/payment/dto/response/PaymentCardResponse.java
@@ -12,4 +12,13 @@ public record PaymentCardResponse(
         String acquireStatus,
         String receiptUrl
 ) {
+    public static PaymentCardResponse of(
+            String company, String number, String installmentPlanMonths, String isInterestFree, String approveNo,
+            String useCardPoint, String cardType, String ownerType, String acquireStatus, String receiptUrl
+    ) {
+        return new PaymentCardResponse(
+                company, number, installmentPlanMonths, isInterestFree, approveNo, useCardPoint, cardType, ownerType,
+                acquireStatus, receiptUrl
+        );
+    }
 }
--- server/culinari/src/main/java/com/codestates/culinari/payment/dto/response/PaymentSuccessResponse.java
+++ server/culinari/src/main/java/com/codestates/culinari/payment/dto/response/PaymentSuccessResponse.java
@@ -20,4 +20,16 @@ public record PaymentSuccessResponse(
         PaymentCardResponse card,
         String type
 ) {
+
+    public static PaymentSuccessResponse of(
+            String mId, String version, String paymentKey, String orderId, String orderName, String currency,
+            String method, String totalAmount, String balanceAmount, String suppliedAmount, String vat, String status,
+            String requestedAt, String approvedAt, String useEscrow, String cultureExpense, PaymentCardResponse card,
+            String type
+    ) {
+        return new PaymentSuccessResponse(
+                mId, version, paymentKey, orderId, orderName, currency, method, totalAmount, balanceAmount,
+                suppliedAmount, vat, status, requestedAt, approvedAt, useEscrow, cultureExpense, card, type
+        );
+    }
 }
