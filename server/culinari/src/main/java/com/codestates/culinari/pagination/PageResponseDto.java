884faae61ef5a295835458a5d7fc5172b012e7d8
sGOM <pooh6195@naver.com>
2023-01-23 22:08:32 +0900

    #95 - refactor: 사용하지 않거나 작동하지 않는 코드 삭제

--- server/culinari/src/main/java/com/codestates/culinari/pagination/PageResponseDto.java
+++ server/culinari/src/main/java/com/codestates/culinari/pagination/PageResponseDto.java
@@ -10,10 +10,8 @@ import java.util.List;
 public class PageResponseDto<T> {
     private List<T> data;
     private PageInfo pageInfo;
-    private Page<T> pageData;
-    private Pageable pageable;
     private List<Integer> barNumber;
-                                          ///int page, size
+
     public PageResponseDto(List<T> data, Page page, List<Integer> barNumber) {
         this.data = data;
         this.pageInfo = new PageInfo(page.getNumber() + 1,
@@ -25,12 +23,5 @@ public class PageResponseDto<T> {
         this.data = data;
         this.pageInfo = new PageInfo(page.getNumber() + 1,
                 page.getSize(), page.getTotalElements(), page.getTotalPages());
-    }
-
-    //페이지네이션의 호출 방식 변경 시 활용
-    public PageResponseDto(List<T> data, Pageable pageable, List<Integer> barNumber){
-        this.data = data;
-        this.pageable = pageable;
-        this.barNumber = barNumber;
     }
 }
