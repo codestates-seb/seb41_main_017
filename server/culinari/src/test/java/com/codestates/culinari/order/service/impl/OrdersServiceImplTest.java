c21d095eb71a91c972b791f4fc6e7f20e35301de
Heo SeokJin <pooh6195@naver.com>
2023-01-23 16:18:41 +0900

    [BE] 결제 기능 테스트 작성 (#94)
    * #88 - 테스트를 위한 `Stub` 데이터 작성
    
    * #88 - 결제 관련 테스트 작성
    
    - toss API 요청을 보내는 메서드를 제외하고 테스트를 진행함
    
    * #88 - 기능 변경에 대한 `Orders` 테스트 삭제
    
    * EOF 추가

--- server/culinari/src/test/java/com/codestates/culinari/order/service/impl/OrdersServiceImplTest.java
+++ server/culinari/src/test/java/com/codestates/culinari/order/service/impl/OrdersServiceImplTest.java
@@ -1,10 +1,7 @@
 package com.codestates.culinari.order.service.impl;
 
 import com.codestates.culinari.config.security.dto.CustomPrincipal;
-import com.codestates.culinari.order.repository.CartRepository;
-import com.codestates.culinari.order.repository.OrderDetailRepository;
 import com.codestates.culinari.order.repository.OrdersRepository;
-import com.codestates.culinari.user.repository.ProfileRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,59 +29,6 @@ class OrdersServiceImplTest {
     private OrdersServiceImpl sut;
     @Mock
     private OrdersRepository ordersRepository;
-    @Mock
-    private OrderDetailRepository orderDetailRepository;
-    @Mock
-    private CartRepository cartRepository;
-    @Mock
-    private ProfileRepository profileRepository;
-
-//    @DisplayName("주문에 필요한 정보를 입력하면, 주문을 등록한다.")
-//    @Test
-//    void givenNewOrderInfoAndPrincipal_whenSavingOrder_thenSaveOrder() {
-//        // Given
-//        OrderRequest orderRequest = createOrderRequest();
-//        CustomPrincipal principal = createPrincipal("사용자 명", 1L, 1L);
-//
-//        given(profileRepository.getReferenceById(anyLong())).willReturn(createProfile(1L));
-//        given(ordersRepository.save(any(Orders.class))).willReturn(createOrder(1L, 1L));
-//        given(cartRepository.findByProfile_IdAndProduct_Id(anyLong(), anyLong())).willReturn(Optional.of(createCart(1L, 1, 1L, 1L)));
-//        willDoNothing().given(cartRepository).delete(any(Cart.class));
-//        given(orderDetailRepository.save(any(OrderDetail.class))).willReturn(createOrderDetail(1L));
-//
-//        // When
-//        sut.createOrder(orderRequest, principal);
-//
-//        // Then
-//        then(profileRepository).should().getReferenceById(anyLong());
-//        then(ordersRepository).should().save(any(Orders.class));
-//        verify(cartRepository, times(orderRequest.productIds().size())).findByProfile_IdAndProduct_Id(anyLong(), anyLong());
-//        verify(cartRepository, times(orderRequest.productIds().size())).delete(any(Cart.class));
-//        verify(orderDetailRepository, times(orderRequest.productIds().size())).save(any(OrderDetail.class));
-//    }
-//
-//    @DisplayName("잘못된 주문 정보를 입력하면, 예외를 반환한다.")
-//    @Test
-//    void givenNewOrderInfoWithNonexistentCartIdAndPrincipal_whenSavingOrder_thenThrowsException() {
-//        // Given
-//        OrderRequest orderRequest = createOrderRequest();
-//        CustomPrincipal principal = createPrincipal("사용자 명", 1L, 1L);
-//
-//        given(profileRepository.getReferenceById(anyLong())).willReturn(createProfile(1L));
-//        given(ordersRepository.save(any(Orders.class))).willReturn(createOrder(1L, 1L));
-//        given(cartRepository.findByProfile_IdAndProduct_Id(anyLong(), anyLong())).willReturn(Optional.empty());
-//
-//        // When
-//        Throwable t = catchThrowable(() -> sut.createOrder(orderRequest, principal));
-//
-//        // Then
-//        assertThat(t)
-//                .isInstanceOf(BusinessLogicException.class)
-//                .hasMessage("Cart not found");
-//        then(profileRepository).should().getReferenceById(anyLong());
-//        then(ordersRepository).should().save(any(Orders.class));
-//        then(cartRepository).should().findByProfile_IdAndProduct_Id(anyLong(), anyLong());
-//    }
 
     @DisplayName("주문을 조회할 기간을 입력하면, 기간내에 주문한 주문 목록을 반환한다.")
     @Test
