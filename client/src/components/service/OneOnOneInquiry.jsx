e374239b9815a7f1cdda8dbf8b582afd2a4a6c6d
junpotatoes <mukdol1127@naver.com>
2023-01-24 11:39:43 +0900

    #62 - design: 1:1문의사항 inquiry css 변경

--- client/src/components/service/OneOnOneInquiry.jsx
+++ client/src/components/service/OneOnOneInquiry.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import BasicInput from "../BasicInput";
 import BasicButton from "../BasicButton";
 import { BsTextarea } from "react-icons/bs";
-const InquiryCotainer = styled.div`
+import { useState } from "react";
+const InquiryCotainer = styled.form`
   width: 900px;
 
   h2 {
@@ -12,39 +13,56 @@ const InquiryCotainer = styled.div`
   .content_container {
     width: 100%;
     margin-top: 20px;
+    margin-bottom: 10px;
   }
 
   .btn_container {
+    margin-top: 20px;
     display: flex;
     align-item: center;
     justify-content: right;
   }
+
+  textarea {
+    width: 100%;
+
+    height: 15.25em;
+  }
+  .title_content {
+    width: 100%;
+    height: 35px;
+  }
 `;
 
 function OneOnOneInquiry() {
+  const [question, setQuestion] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setQuestion(...setQuestion);
+  };
+
   return (
     <InquiryCotainer>
       <div className="content_container">
         <h2>문의 작성</h2>
       </div>
       <div className="content_container">
-        <BasicInput
-          label={"글 제목"}
-          width={"100%"}
-          star={"*"}
-          placeholder="제목을 입력해주세요"
-        ></BasicInput>
+        <label htmlFor="title">글 제목</label>
       </div>
+      <input
+        className="title_content"
+        type="text"
+        placeholder="  제목을 입력해주세요"
+      />
       <div className="content_container">
-        <BasicInput
-          label={"글 내용"}
-          width={"100%"}
-          height={"300px"}
-          type={"textarea"}
-          star={"*"}
-          placeholder="내용을 입력해주세요"
-        ></BasicInput>
+        <label htmlFor="content">글 내용</label>
       </div>
+      <textarea placeholder="내용을 입력해주세요"></textarea>
       <div className="btn_container">
         <BasicButton p_width={"20"} p_height={"7"}>
           문의 작성
