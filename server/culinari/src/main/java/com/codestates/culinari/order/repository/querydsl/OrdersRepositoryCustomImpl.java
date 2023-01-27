c341fc33c9ff617da9ba669d48977f01163c3649
sGOM <pooh6195@naver.com>
2023-01-27 12:45:32 +0900

    #138 - `Query`에 검증 로직 추가

--- server/culinari/src/main/java/com/codestates/culinari/order/repository/querydsl/OrdersRepositoryCustomImpl.java
+++ server/culinari/src/main/java/com/codestates/culinari/order/repository/querydsl/OrdersRepositoryCustomImpl.java
@@ -2,7 +2,9 @@ package com.codestates.culinari.order.repository.querydsl;
 
 import com.codestates.culinari.order.entitiy.Orders;
 import com.codestates.culinari.order.entitiy.QOrders;
+import com.codestates.culinari.payment.entity.QPayment;
 import com.codestates.culinari.user.entitiy.QProfile;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -21,14 +23,15 @@ public class OrdersRepositoryCustomImpl extends QuerydslRepositorySupport implem
     @Override
     public Page<Orders> findAllCreatedAfterAndProfile_Id(LocalDateTime createdAfterDateTime, Long profileId, Pageable pageable) {
         QOrders order = QOrders.orders;
+        QPayment payment = QPayment.payment;
         QProfile profile = QProfile.profile;
 
         JPQLQuery<Orders> query =
                 from(order)
                         .innerJoin(order.profile, profile).fetchJoin()
                         .where(order.createdAt.gt(createdAfterDateTime)
-                                .and(profile.id.eq(profileId)))
-                        .orderBy(order.createdAt.desc());
+                                .and(profile.id.eq(profileId))
+                                .and(order.id.in(JPAExpressions.select(payment.order.id).from(payment).where(payment.paySuccessTf.eq(true)))));
         List<Orders> orders = getQuerydsl().applyPagination(pageable, query).fetch();
 
         return new PageImpl<>(orders, pageable, query.fetchCount());
