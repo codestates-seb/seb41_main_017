ab74a5d71cae6b2dc34f5b73fc664df38f8950c6
junpotatoes <mukdol1127@naver.com>
2023-01-29 02:57:22 +0900

    #175 - feat: TossPayments 결제 기능 구현

--- client/src/components/pay/index.js
+++ client/src/components/pay/index.js
@@ -7,6 +7,7 @@ import OrderInfo from "./OrderInfo";
 import ShipInfo from "./ShipInfo";
 import PayInfo from "./PayInfo";
 import InfoCheck from "./InfoCheck";
+import { loadTossPayments } from "@tosspayments/payment-sdk";
 
 const Container = styled.div`
   max-width: 1050px;
@@ -47,6 +48,66 @@ const ButtonWrapper = styled.div`
 `;
 
 function Pay() {
+  const ProductIds = localStorage.getItem("productIds");
+
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState("");
+
+  const handlePayBtnClick = async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token");
+    const parse = JSON.parse(token);
+    const header = {
+      headers: {
+        "Content-Type": `application/json`,
+        authorization: parse.authorization,
+      },
+    };
+
+    let data = {
+      payType: "CARD",
+      productIds: ProductIds.slice(1, ProductIds.length - 1).split(","),
+      address: filterData[0].address,
+      receiverName: filterData[0].receiverName,
+      receiverPhoneNumber: filterData[0].receiverPhoneNumber,
+    };
+
+    const clientKey = "test_ck_jkYG57Eba3G7GdKlLJL3pWDOxmA1"; // 고정 값입니다. 환경변수나 git에 올라가지 않는 파일로 빼주시면 감사하겠습니다.
+    const res = await axios.post(`${BASE_URL}/payments`, data, header);
+
+    const tossPayments = await loadTossPayments(clientKey);
+
+    tossPayments.requestPayment(res.data.data.payType, {
+      amount: res.data.data.amount,
+      orderId: res.data.data.orderId,
+      orderName: res.data.data.orderName,
+      customerName: "test",
+      successUrl: res.data.data.successUrl,
+      failUrl: res.data.data.failUrl,
+    });
+  };
+
+  const fetchData = () => {
+    axios
+      .get(`${BASE_URL}/destination`, {
+        headers: {
+          "Content-Type": `application/json`,
+          authorization: JSON.parse(localStorage.getItem("token"))
+            .authorization,
+        },
+      })
+      .then((res) => {
+        setData(res.data.data);
+        setFilterData(res.data.data.filter((el) => el.defaultSelect === true));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Container>
       <TitleContainer>
@@ -57,7 +118,7 @@ function Pay() {
       <PayInfo />
       <InfoCheck />
 
-      <ButtonWrapper>
+      <ButtonWrapper onClick={handlePayBtnClick}>
         <BasicButton
           children={"결제하기"}
           font={"20"}
