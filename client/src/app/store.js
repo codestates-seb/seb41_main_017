7f97c2efa94e69bf3737478a85d1a4062e6152e5
jsh3418 <albert5428@gmail.com>
2023-01-29 01:10:07 +0900

    #171 - feat: store에 reducer 추가

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productIdsReducer from "./reducer/productId2Pay";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    productIds: productIdsReducer,
+  },
 });
