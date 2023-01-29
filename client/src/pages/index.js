3ce0fa7efbe41315ebbcd1bbd5cd999b6d504c89
Seonghun <albert5428@gmail.com>
2023-01-30 00:27:26 +0900

    [FE] ProductItemSlider 상품 리스트 수정 (#196)
    * #195 - feat: ProductItemSlider 상품 리스트 수정
    
    * #195 - design: 슬라이더 버튼 커서 수정

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,11 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+
 import MainBanner from "../components/MainBanner";
 import ProductItemSlider from "../components/ProductItemSlider";
+
+import BASE_URL from "../constants/BASE_URL";
 
 const Container = styled.div`
   min-width: 1050px;
@@ -20,18 +25,47 @@ export const Title = styled.div`
 `;
 
 export function Main() {
+  const [newProductData, setNewProductData] = useState(null);
+  const [bestProductData, setBestProductData] = useState(null);
+
+  useEffect(() => {
+    const getNewProductData = async () => {
+      const { data } = await axios.get(`${BASE_URL}/collections/newproduct?size=20`);
+
+      return data;
+    };
+
+    const getBestProductData = async () => {
+      const { data } = await axios.get(`${BASE_URL}/collections/bestproducts?size=20`);
+
+      return data;
+    };
+
+    (async () => {
+      try {
+        const newProductData = await getNewProductData();
+        const bestProductData = await getBestProductData();
+
+        setNewProductData(newProductData);
+        setBestProductData(bestProductData);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
     <Container>
       <MainBanner />
 
       <TodayRecommendProducts>
         <Title>이달의 추천 상품</Title>
-        <ProductItemSlider />
+        <ProductItemSlider data={bestProductData && bestProductData.data} />
       </TodayRecommendProducts>
 
       <TodayRecommendProducts>
         <Title>이달의 신상품</Title>
-        <ProductItemSlider />
+        <ProductItemSlider data={newProductData && newProductData.data} />
       </TodayRecommendProducts>
     </Container>
   );
