91acfe0b23ea764ea730fe788ddbca2ccd98b113
Seonghun <albert5428@gmail.com>
2023-01-29 17:01:46 +0900

    [FE] 결제페이지 새로고침 시 상태 데이터 초기화되는 현상 수정 (#184)
    * #183 - chore: redux-persist 패키지 추가
    
    * #183 - feat: redux-persist 적용 새로고침 시 상태 초기화 이슈 fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
