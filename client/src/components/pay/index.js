273a35d337e922241103487817c98ca67e25c011
최준호 <103482590+junpotatoes@users.noreply.github.com>
2023-01-29 17:46:10 +0900

    [FE] 결제 페이지 Toss 결제 기능 구현 (#185)
    * #175 -  chore: TossPayments 설치
    
    * #175 - feat: TossPayments 결제 기능 구현
    
    * #175 - feat: 결제 페이지에서 userData 가져오기
    
    * #175 - feat: 결제 페이지에서 filterData 가져오기
    
    * #175 - feat: 결제 페이지에서 price 가져오기
    
    * #175 - comment: console.log 삭제
    
    * #175 - feat: InfoCheck 컴포넌트 reject 상태 전달하기

--- client/src/components/pay/index.js
+++ client/src/components/pay/index.js
@@ -7,7 +7,16 @@ import OrderInfo from "./OrderInfo";
 import ShipInfo from "./ShipInfo";
 import PayInfo from "./PayInfo";
 import InfoCheck from "./InfoCheck";
+import { loadTossPayments } from "@tosspayments/payment-sdk";
 import { useSelector } from "react-redux";
+import {
+  ModalTitle,
+  ModalText,
+  CloseButton,
+  ButtonWrapper,
+  ModalWrapper,
+  SignupFailureContainer,
+} from "../../styles/signupStyle";
 
 const Container = styled.div`
   max-width: 1050px;
@@ -32,37 +41,124 @@ const TitleContainer = styled.div`
   }
 `;
 
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 8px;
-  border-top: 1px solid rgb(244, 244, 244);
-  text-align: center;
-  padding-top: 20px;
-  margin-top: 20px;
-
-  a {
-    background-color: #ffffff;
-    border-radius: 3px;
-  }
-`;
-
 function Pay() {
-  const productsInfo = useSelector((state) => state.productsInfo);
+  const productIds = useSelector((state) => state.productsInfo);
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState("");
+  const [userData, setUserData] = useState("");
+  const [price, setPrice] = useState(productIds);
+  const [rejectModal, setRecjectModal] = useState(false);
+  const [reject, setReject] = useState(false);
 
-  console.log(productsInfo);
+  const handlePayBtnClick = async (e) => {
+    e.preventDefault();
+
+    const token = localStorage.getItem("token");
+    const parse = JSON.parse(token);
+    const header = {
+      headers: {
+        "Content-Type": `application/json`,
+        authorization: parse.authorization,
+      },
+    };
+
+    let data = {
+      payType: "CARD",
+      productIds: productIds.info.ids,
+      address: filterData[0].address,
+      receiverName: filterData[0].receiverName,
+      receiverPhoneNumber: filterData[0].receiverPhoneNumber,
+    };
+
+    const clientKey = "test_ck_jkYG57Eba3G7GdKlLJL3pWDOxmA1";
+
+    if (reject === false) {
+      setRecjectModal(true);
+    } else {
+      const res = await axios.post(`${BASE_URL}/payments`, data, header);
+
+      const tossPayments = await loadTossPayments(clientKey);
+
+      tossPayments.requestPayment(res.data.data.payType, {
+        amount: res.data.data.amount,
+        orderId: res.data.data.orderId,
+        orderName: res.data.data.orderName,
+        customerName: "test",
+        successUrl: res.data.data.successUrl,
+        failUrl: res.data.data.failUrl,
+      });
+    }
+  };
+
+  // 배송지 정보 (배송지 수령인, 수령인 번호, 수령인 주소)
+  const fetchData = () => {
+    axios
+      .get(`${BASE_URL}/destination`, {
+        headers: {
+          "Content-Type": `application/json`,
+          authorization: JSON.parse(localStorage.getItem("token"))
+            .authorization,
+        },
+      })
+      .then((res) => {
+        setData(res.data.data);
+        setFilterData(res.data.data.filter((el) => el.defaultSelect === true));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // 유저 정보 (사용자 이름, 사용자 번호, 사용자 이메일)
+  const userFetchData = () => {
+    axios
+      .get(`${BASE_URL}/users`, {
+        headers: {
+          "Content-Type": `application/json`,
+          authorization: JSON.parse(localStorage.getItem("token"))
+            .authorization,
+        },
+      })
+      .then((res) => setUserData(res.data.data))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    userFetchData();
+  }, []);
+
   return (
     <Container>
       <TitleContainer>
         <h2>결제하기</h2>
       </TitleContainer>
-      <OrderInfo />
-      <ShipInfo />
-      <PayInfo />
-      <InfoCheck />
+      <OrderInfo userData={userData} />
+      <ShipInfo filterData={{ filterData }} />
+      <PayInfo price={price} />
+      <InfoCheck setReject={setReject} reject={reject} />
 
-      <ButtonWrapper>
-        <BasicButton children={"결제하기"} font={"20"} radius={"5"} p_height={"14"} p_width={"150"} />
+      {rejectModal && (
+        <ModalWrapper>
+          <SignupFailureContainer>
+            <ModalTitle>결제 실패</ModalTitle>
+            <ModalText>
+              이용약관과 개인정보 수집 이용동의를 모두 동의해주세요.
+            </ModalText>
+          </SignupFailureContainer>
+          <CloseButton onClick={() => setRecjectModal(false)}>닫기</CloseButton>
+        </ModalWrapper>
+      )}
+
+      <ButtonWrapper onClick={handlePayBtnClick}>
+        <BasicButton
+          children={"결제하기"}
+          font={"20"}
+          radius={"5"}
+          p_height={"14"}
+          p_width={"150"}
+        />
       </ButtonWrapper>
     </Container>
   );
