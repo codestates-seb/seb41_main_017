5da1b7040d0613cba3c8a6e65c4964c1e4e53568
jsh3418 <albert5428@gmail.com>
2023-01-31 02:51:10 +0900

    #213 - chore: 안내 메세지 수정

--- client/src/pages/productDetail/productAtf/ProductHeader.jsx
+++ client/src/pages/productDetail/productAtf/ProductHeader.jsx
@@ -45,7 +45,7 @@ function ProductHeader({ data }) {
     const accessToken = JSON.parse(localStorage.getItem("token"))?.authorization;
 
     if (!accessToken) {
-      if (window.confirm("찜 기능은 로그인 후에 가능합니다. 로그인 페이지으로 이동하시겠습니까?")) {
+      if (window.confirm("찜 기능은 로그인 후에 사용할 수 있습니다. 로그인 페이지으로 이동하시겠습니까?")) {
         navigate("/login");
 
         return;
