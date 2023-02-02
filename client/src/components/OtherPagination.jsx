bc6a12a20751ad9d1bc13761ce5ae527e0dd41de
최준호 <103482590+junpotatoes@users.noreply.github.com>
2023-02-02 14:53:08 +0900

    #223 - feat: 1:1문의 컴포넌트 페이지네이션 구현 (#236)

--- client/src/components/OtherPagination.jsx
+++ client/src/components/OtherPagination.jsx
@@ -1,17 +1,16 @@
 import styled from "styled-components";
 
 const Layout = styled.div`
-    justify-content: center;
-    display: flex;
-    font-family: 'Times New Roman', Times, serif;
+  justify-content: center;
+  display: flex;
+  font-family: "Times New Roman", Times, serif;
 
-    & > div:first-child{
-        border-radius:10px 0px 0px 10px;
-    }
-    & > div:last-child{
-        border-radius:0px 10px 10px 0px;
-    }
-
+  & > div:first-child {
+    border-radius: 10px 0px 0px 10px;
+  }
+  & > div:last-child {
+    border-radius: 0px 10px 10px 0px;
+  }
 
   .box {
     display: flex;
@@ -19,22 +18,19 @@ const Layout = styled.div`
     height: 100%;
     background-color: #ffeaea;
 
-
     span {
-      padding: 10px
+      padding: 10px;
       display: inline-block;
       cursor: pointer;
       border-radius: 8px;
 
-      &:hover{
+      &:hover {
         background-color: #ff6767;
         color: white;
         border-radius: 8px;
       }
     }
   }
-
-
 
   .current {
     background-color: #ff6767;
@@ -45,7 +41,7 @@ const Layout = styled.div`
 export const OtherPagination = ({ state, setState, pageInfo }) => {
   const pageData = pageInfo;
   const left = () => {
-    if (state === 1) {
+    if (state === 0) {
       return setState(0);
     } else {
       setState(state - 1);
@@ -64,32 +60,27 @@ export const OtherPagination = ({ state, setState, pageInfo }) => {
 
   return (
     <Layout>
-        {state === 0 ? null : (
-          <div className="box" onClick={left}>
-            <span>{"<"}</span>
-          </div>
-        )}
-        <div className="box">
-          {Array(pageData?.totalPages)
-            .fill()
-            .map((el, idx) => {
-              return (
-                <span
-                  onClick={(e) => clickNum(e.target.innerText)}
-                  className={`${state === idx ? "current" : "simple"}`}
-                  key={idx}
-                >
-                  {idx + 1}
-                </span>
-              );
-            })}
-        </div>
-        {pageData?.totalPages === 0 ? null : state ===
-          pageData?.totalPages - 1 ? null : (
-          <div className="box" onClick={right}>
-            <span>{">"}</span>
-          </div>
-        )}
+      <div className="box" onClick={left}>
+        <span>{"<"}</span>
+      </div>
+      <div className="box">
+        {Array(pageData?.totalPages)
+          .fill()
+          .map((el, idx) => {
+            return (
+              <span
+                onClick={(e) => clickNum(e.target.innerText)}
+                className={`${state === idx ? "current" : "simple"}`}
+                key={idx}
+              >
+                {idx + 1}
+              </span>
+            );
+          })}
+      </div>
+      <div className="box" onClick={right}>
+        <span>{">"}</span>
+      </div>
     </Layout>
   );
 };
