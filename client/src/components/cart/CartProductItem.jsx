4214b81a8e3b4ccdca46cb90370df0f31ecf34ac
jsh3418 <albert5428@gmail.com>
2023-02-02 20:53:18 +0900

    #250 - feat: 장바구니 상품 리스트 제목에 링크 연결

--- client/src/components/cart/CartProductItem.jsx
+++ client/src/components/cart/CartProductItem.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import BASE_URL from "../../constants/BASE_URL";
 import CheckBox from "../CheckBox";
@@ -36,6 +37,7 @@ const ProductInfo = styled.div`
 
 const ProductTitle = styled.div`
   margin-bottom: 5px;
+  cursor: pointer;
 `;
 
 const ProductPrice = styled.div`
@@ -58,6 +60,7 @@ const DeleteButtonWrapper = styled.div`
 function CartProductItem({ item, data, setData, index, checkedList, setCheckedList, selectAllChecked }) {
   const [quantity, setQuantity] = useState(item.quantity);
   const [isChecked, setIsChecked] = useState(!!checkedList.find((element) => element.id === item.id));
+  const navigate = useNavigate();
 
   useEffect(() => {
     const patchQuantity = () => {
@@ -127,6 +130,10 @@ function CartProductItem({ item, data, setData, index, checkedList, setCheckedLi
     setIsChecked(!isChecked);
   };
 
+  const handleProductNameAndImgClick = () => {
+    navigate(`/product/${data.data[index].product.id}`);
+  };
+
   return (
     <Container>
       <div onClick={handleCheckBoxClick}>
@@ -136,7 +143,7 @@ function CartProductItem({ item, data, setData, index, checkedList, setCheckedLi
       <ImageWrapper>{<img src={item.product.productImageDtos[0].imgUrl} />}</ImageWrapper>
 
       <ProductInfo>
-        <ProductTitle>{item.product.name}</ProductTitle>
+        <ProductTitle onClick={handleProductNameAndImgClick}>{item.product.name}</ProductTitle>
         <ProductPrice>{item.product.price.toLocaleString()}원</ProductPrice>
         <QuantityBoxWrapper>
           <QuantityBox quantity={quantity} setQuantity={setQuantity} />
